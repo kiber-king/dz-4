d84de7242b4f189f2e8e6d8127a42d84898c5be3
Gleb <gleb.kvasov26@mail.ru>
2022-04-20 13:53:30 +0300

    Добавьте файлы проекта.

---
+++ дз 4/дз 4.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    double a = 3.5;
+    double x = -0.7;
+    double y = 0;
+    double t = 0;
+
+    if (a <= x) {
+        y=a + log(x + a);
+    }
+    else if (a>x){
+        if(a*x>=0){
+            y =sqrt(abs(sin(a * x)));
+        }
+        else if (a * x < 0) {
+            cout << "Err";
+            return 0;
+        }
+    }
+
+    if (a > y) {
+        t = y /(a-x);
+    }
+    else if (a == y) {
+        t = y/(a-x) + (a+x)/pow(y,2);
+    }
+    else if (a < y) {
+        t = tan(a * x) + cos(2*a*y);
+    }
+    cout << "a: " << a << "\n" << "x: " << x << "\n" << "y: " << y << "\n"
+        << "t: " << t << "\n";
+}
+
